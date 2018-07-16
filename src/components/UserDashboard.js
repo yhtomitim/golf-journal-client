115357fe0a2e1f3c238608d85f50ea777a26dafd
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-16 10:15:41 -0600

    add a sanity check user dashboard component

---
+++ src/components/UserDashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = (props) => {
+  return (
+    <section>
+      <h1>A User's stuff will show up here!</h1>
+    </section>
+  )
+}
+
+export default Dashboard;
