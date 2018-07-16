7e2ffa4b49baf32403e430c2725e0fb15867a9db
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-16 14:17:26 -0600

    create component for demo view

---
+++ src/components/Demo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Demo = () => {
+  return (
+    <section>
+      <p>this will be a demo video of how the app works</p>
+    </section>
+  )
+}
+
+export default Demo;
