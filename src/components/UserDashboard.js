6fb28fec2ae03872066afd64a6307b7d6186cd76
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-16 10:35:12 -0600

    create some placeholder tags for dashboard items

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -4,6 +4,11 @@ const Dashboard = (props) => {
   return (
     <section>
       <h1>A User's stuff will show up here!</h1>
+      <h3>Welcome Username Here</h3>
+      <div className="Rounds-tracked">
+        <article>latest round of golf</article>
+        <article>other rounds of golf tracked</article>
+      </div>
     </section>
   )
 }
