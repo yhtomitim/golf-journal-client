bb683139ef4c4903f0f2c0e08915b5315c847d19
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-16 10:16:33 -0600

    add hero image, remove template image import authexample component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
-import logo from '../logo.svg';
+import heroImage from '../header.jpg';
+import AuthExample from './Auth';
 
 const Header = (props) => {
   return (
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-      <nav className="Navigation"></nav>
+      <img src={heroImage} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to My Golf Journal</h1>
+      <AuthExample />
     </header>
   );
 };
