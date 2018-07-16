713fc511cb1920bfc0571ec73aba7b1f466d6949
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-16 10:21:42 -0600

    add dashboard component for functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import UserDashboard from './components/UserDashboard'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <Header />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <UserDashboard />
+        <Footer />
       </div>
     );
   }
