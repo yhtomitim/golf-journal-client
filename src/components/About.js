3f4ebbc3bca3b1bd05e906682ff61531ecb8cb36
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-16 14:16:31 -0600

    add component for app intro

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <main className="App-intro">
+      <p>
+        This web app will allow a golfer to take notes on his or her round.The golfer can select from nearby golf courses and retrieve relevant info on the selected course like if they have a driving range.
+      </p>
+      <p>
+        Problem Solved with App: This app solves a problem for golfers who want a place to keep their score data and notes on how they achieved that score in one place.For instance, a score on a hole only tells part of the story— what if that double-bogey was the result of a penalty and a poor chip ? Leaving notes on how a golfer performs on each hole can dramatically improve the golfer’ s game round to round.
+      </p>
+      <p>
+        How it Solves this problem : Allows for a golfer to record their scores on a hole as well as notes on each shot or how they played the hole overall.These notes can be viewed later to help a golfer improve areas of their game round-to-round.
+      </p>
+    </main>
+  )
+}
+
+export default About;
