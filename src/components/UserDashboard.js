3a5b2e2b5b6590a32681d8a31ee017ba36489126
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-19 08:27:30 -0600

    refactor userdashboard to classful component and add click event

--- src/components/UserDashboard.js
+++ src/components/UserDashboard.js
@@ -5,19 +5,36 @@ import {
 } from 'react-router-dom'
 import RoundTracker from './RoundTracker';
 
-const UserDashboard = () => {
-  return (
-    <section>
-      <h3>Welcome Username Here</h3>
-      <div className="Rounds-tracked">
-        <article>latest round of golf</article>
-        <article>other rounds of golf tracked</article>
-      </div>
-      <Link to='/protected/round'>Start a New Round</Link>
-      <Route path='/protected/round' component={RoundTracker}/>
-      {/* <RoundTracker /> */}
-    </section>
-  )
+class UserDashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      uid: 'thurley'
+    }
+    this.createNewRound = this.createNewRound.bind(this);
+  }
+  createNewRound(event) {
+    event.preventDefault();
+    console.log('clicked this');
+    
+  }
+  render() {
+    return (
+      <section>
+        <h3>Welcome Username Here</h3>
+        <div className="Rounds-tracked">
+          <article>latest round of golf</article>
+          <article>other rounds of golf tracked</article>
+        </div>
+        <Link
+          to='/protected/round'
+          onClick={this.createNewRound}>Start a New Round</Link>
+        <Route path='/protected/round' component={RoundTracker}/>
+        {/* <RoundTracker /> */}
+      </section>
+    )
+  }
 }
+
 
 export default UserDashboard;
