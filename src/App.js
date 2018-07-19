ff1a857e8225ac8da1aa701c6bec5515adcb0817
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-19 11:54:41 -0600

    refactor to include best practice of router use

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
-import Router from './Router'
+import Router from './components/Router'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import About from './components/About'
+import Main from './components/Main'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <About />
-        <Router />
-        <Footer />
-      </div>
+      // <Router>
+      //   Route
+      //   Route
+      //   Route component demo
+      //   route component login
+      //     route component protected
+      //       route component new round
+      // </Router>
+      <Router>
+        <div className="App">
+          <Header />
+          <Main />
+          <About />
+          <Footer />
+          >
+        </div>
+      </Router>
     );
   }
 }
