e5b24e42c056f2d00e2a3419e440e68f296f64a7
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-23 15:32:10 -0600

    remove comments

--- src/components/AuthComponent.js
+++ src/components/AuthComponent.js
@@ -8,12 +8,6 @@ import {
 } from "react-router-dom";
 import UserDashboard from './UserDashboard';
 
-////////////////////////////////////////////////////////////
-// 1. Click the public page
-// 2. Click the protected page
-// 3. Log in
-// 4. Click the back button, note the URL each time
-
 const AuthComponent = () => (
   <Router>
     <div>
