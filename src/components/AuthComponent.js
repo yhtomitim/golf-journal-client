39e66d7d411b0ab3b465314c06d71a8858f7ce70
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-23 15:23:07 -0600

    rename component

---
+++ src/components/AuthComponent.js
@@ -0,0 +1,120 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from "react-router-dom";
+import UserDashboard from './UserDashboard';
+
+////////////////////////////////////////////////////////////
+// 1. Click the public page
+// 2. Click the protected page
+// 3. Log in
+// 4. Click the back button, note the URL each time
+
+const AuthComponent = () => (
+  <Router>
+    <div>
+      <AuthButton />
+      <Link to="/dashboard">User Dashboard</Link>
+      <Route path="/login" component={Login} />
+      <PrivateRoute path="/dashboard" component={UserDashboard} />
+    </div>
+  </Router>
+);
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true;
+    setTimeout(cb, 100); // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false;
+    setTimeout(cb, 100);
+  }
+};
+
+const AuthButton = withRouter(
+  ({ history }) =>
+    fakeAuth.isAuthenticated ? (
+      <p>
+        Welcome!{" "}
+        <button
+          onClick={() => {
+            fakeAuth.signout(() => history.push("/"));
+          }}
+        >
+          Sign out
+        </button>
+      </p>
+    ) : (
+      <p>You are not logged in.</p>
+    )
+);
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      fakeAuth.isAuthenticated ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/login",
+            state: { from: props.location }
+          }}
+        />
+      )
+    }
+  />
+);
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirectToReferrer: false,
+      user_id: '' //no snake case so as to match data column
+    };
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+    console.log(this.state);
+  }
+
+  login = () => {
+    fakeAuth.authenticate(() => {
+      this.setState({ redirectToReferrer: true });
+    });
+  };
+
+  render() {
+    const { from } = this.props.location.state || { from: { pathname: "/dashboard" } };
+    const { redirectToReferrer } = this.state;
+
+    if (redirectToReferrer) {
+      return <Redirect to={from} />;
+    }
+
+    return (
+      <div>
+        <p>You must log in to view the Dashboard at {from.pathname}</p>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          name="user_id"
+          value={this.state.user_id}
+          onChange={this.handleChange}/>
+        <button onClick={this.login}>Log in</button>
+      </div>
+    );
+  }
+}
+
+export default AuthComponent;
