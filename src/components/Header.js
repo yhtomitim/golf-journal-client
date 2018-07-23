912d878c2e2e1fb0bc03e80d176e9c5bf90a3d50
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-23 11:03:21 -0600

    import link from router add link tags for routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import React from "react";
 import heroImage from '../header.jpg';
-// import AuthExample from './Auth';
+import { Link } from 'react-router-dom';
 
 const Header = (props) => {
   return (
     <header className="App-header">
       <img src={heroImage} className="App-logo" alt="logo" />
       <h1 className="App-title">Welcome to Golf Journal</h1>
-      {/* <AuthExample /> */}
+      <Link to="/demo">Demo</Link> <br/>
+      <Link to="/">Home</Link> <br/>
+      <Link to="/login">Login</Link>
     </header>
   );
 };
