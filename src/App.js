07c5e94e91849107b68ab57b1de3aad63a040929
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-23 15:27:14 -0600

    refactor imported module name

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import About from './components/About';
 import Demo from './components/Demo';
-import Login from './components/AuthComponent';
+import AuthComponent from './components/AuthComponent';
 import './App.css';
 
 class App extends Component {
@@ -14,7 +14,7 @@ class App extends Component {
         <Header />
         <Route exact path="/" component={About} />
         <Route path="/demo" component={Demo} />
-        <Route path="/login" component={Login} />
+        <Route path="/login" component={AuthComponent} />
         <Footer />
       </div>
     );
