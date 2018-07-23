5296485a014967f666a278e8c04fb9c65d665c92
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-23 11:02:17 -0600

    import route from router import components route to login demo about components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
 import React, { Component } from 'react';
-import Router from './components/Router'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import About from './components/About'
-import Main from './components/Main'
+import { Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
+import Demo from './components/Demo';
+import Login from './components/Login';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      // <Router>
-      //   Route
-      //   Route
-      //   Route component demo
-      //   route component login
-      //     route component protected
-      //       route component new round
-      // </Router>
-      <Router>
         <div className="App">
-          <Header />
-          <Main />
-          <About />
-          <Footer />
-          >
+        <Header />
+        <Route exact path="/" component={About} />
+        <Route path="/demo" component={Demo} />
+        <Route path ="/login" component={Login} />
+        <Footer />
         </div>
-      </Router>
     );
   }
 }
