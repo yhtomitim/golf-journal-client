f52a54f0de7648d8ecd5d71b22a882a21fad7df0
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-23 11:56:00 -0600

    complete basic routing for app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import About from './components/About';
 import Demo from './components/Demo';
 import Login from './components/Login';
+import UserDashboard from './components/UserDashboard';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
         <div className="App">
-        <Header />
-        <Route exact path="/" component={About} />
-        <Route path="/demo" component={Demo} />
-        <Route path ="/login" component={Login} />
-        <Footer />
+          <Header />
+          <Route exact path="/" component={About} />
+          <Route path="/demo" component={Demo} />
+          <Route path="/login" component={Login} />
+          <Route path="/dashboard" component={UserDashboard} />
+          <Footer />
         </div>
     );
   }
