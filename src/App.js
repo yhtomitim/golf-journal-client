d6640c6bdaacd53e894cce65d80cce2391cfebf4
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-24 13:57:15 -0600

    add login form to app

--- src/App.js
+++ src/App.js
@@ -9,14 +9,71 @@ import UserDashboard from './components/UserDashboard';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user_id: '', //no snake case so as to match data column
+      isLoggedIn: false
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.getUser = this.getUser.bind(this);
+    this.toggleDashboard = this.toggleDashboard.bind(this);
+    this.updatedToggle = this.updatedToggle.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+    console.log(this.state);
+  }
+  getUser(event) {
+    event.preventDefault();
+    const apiUrl = `http://localhost:8080/api/v1/users/${this.state.user_id}`;
+
+    fetch(apiUrl)
+      .then(res => res.json())
+      .then(res => console.log(res));
+  }
+  toggleDashboard() {
+    this.setState({ isLoggedIn: !this.state.isLoggedIn });
+  }
+
+  updatedToggle(childData) {
+    this.setState({ isLoggedIn: childData });
+  }
+
   render() {
+    if (this.state.isLoggedIn) {
+      return (
+        <div className="App">
+          <Header />
+          {/* <Route path="/" component={About} /> */}
+          <UserDashboard
+            sendToParent={this.updatedToggle}/>
+          <Footer />
+        </div>
+      )
+    }
     return (
       <div className="App">
         <Header />
         <Route exact path="/" component={About} />
-        <Route path="/demo" component={Demo} />
-        <Route path="/login" component={Authentication} />
-        <Route path="/dashboard" component={UserDashboard} />
+        {/* <Route path="/demo" component={Demo} /> */}
+        {/* <Route path="/login" component={Authentication} /> */}
+        <form onSubmit={this.getUser} className="App-intro">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            name="user_id"
+            value={this.state.user_id}
+            onChange={this.handleChange} />
+          <button
+            onClick={this.toggleDashboard}
+            type="submit">Login</button>
+          
+        </form>
+        {/* <Route path="/dashboard" component={UserDashboard} /> */}
         <Footer />
       </div>
     );
