3e453341441a4002eb0c1d013a834e6ae6aac3ee
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-24 08:22:40 -0600

    create component to test firebase auth

---
+++ src/components/Authentication.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+const Authentication = (props) => {
+  return (
+    <div>
+      <div id="firebaseui-auth-container"></div>
+      <p>This is where the "dropped in" auth piece will render</p>
+    </div>
+  )
+}
+
+export default Authentication;
