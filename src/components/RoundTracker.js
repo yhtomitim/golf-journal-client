af4d7903232a9fc0d110eb217068c55c3a514cfd
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-25 10:17:43 -0600

    style and format round tracker form

--- src/components/RoundTracker.js
+++ src/components/RoundTracker.js
@@ -43,54 +43,95 @@ class RoundTracker extends React.Component {
 
   render() {
     return (
-      <form
-        className="Hole-form"
-        onSubmit={this.handleSubmit}>
-          <label htmlFor="hole">Select hole:</label>
-          <select
-            name="hole"
-            value={this.state.hole}
-            onChange={this.handleChange}>
-              <option value="1">No. 1</option>
-              <option value="2">No. 2</option>
-              <option value="3">No. 3</option>
-              <option value="4">No. 4</option>
-              <option value="5">No. 5</option>
-              <option value="6">No. 6</option>
-              <option value="7">No. 7</option>
-              <option value="8">No. 8</option>
-              <option value="9">No. 9</option>
-              <option value="10">No. 10</option>
-              <option value="11">No. 11</option>
-              <option value="12">No. 12</option>
-              <option value="13">No. 13</option>
-              <option value="14">No. 14</option>
-              <option value="15">No. 15</option>
-              <option value="16">No. 16</option>
-              <option value="17">No. 17</option>
-              <option value="18">No. 18</option>
-          </select>
-          <label htmlFor="par">Par:</label>
-          <input
-            type="text"
-            name="par"
-            placeholder="as a number"
-            value={this.state.par}
-            onChange={this.handleChange} />
-          <label htmlFor="score">Your Score:</label>
-          <input
-            type="text"
-            name="score"
-            placeholder="enter your score"
-            value={this.state.score}
-            onChange={this.handleChange} />
-          <label htmlFor="notes">Hole Notes:</label>
-          <textarea
-            name="notes"
-            placeholder="enter notes"
-            value={this.state.notes}
-            onChange={this.handleChange} />
-          <button type="submit">Save Hole</button>
+      <form onSubmit={this.handleSubmit}>
+        <div className="field">
+          <label
+            className="label"
+            htmlFor="hole">Select hole:
+          </label>
+          <div className="control">
+            <div className="select">
+              <select
+                name="hole"
+                value={this.state.hole}
+                onChange={this.handleChange}>
+                  <option value="1">No. 1</option>
+                  <option value="2">No. 2</option>
+                  <option value="3">No. 3</option>
+                  <option value="4">No. 4</option>
+                  <option value="5">No. 5</option>
+                  <option value="6">No. 6</option>
+                  <option value="7">No. 7</option>
+                  <option value="8">No. 8</option>
+                  <option value="9">No. 9</option>
+                  <option value="10">No. 10</option>
+                  <option value="11">No. 11</option>
+                  <option value="12">No. 12</option>
+                  <option value="13">No. 13</option>
+                  <option value="14">No. 14</option>
+                  <option value="15">No. 15</option>
+                  <option value="16">No. 16</option>
+                  <option value="17">No. 17</option>
+                  <option value="18">No. 18</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div className="field">
+          <label
+            className="label"
+            htmlFor="par">Par:
+          </label>
+          <div className="control">
+            <input
+              className="input is-primary is-rounded"
+              type="text"
+              name="par"
+              placeholder="i.e. 4"
+              value={this.state.par}
+              onChange={this.handleChange} />
+          </div>
+        </div>
+        <div className="field">
+          <label
+            className="label"
+            htmlFor="score">Your Score:
+          </label>
+          <div className="control">
+            <input
+              className="input is-primary is-rounded"
+              type="text"
+              name="score"
+              placeholder="enter your score"
+              value={this.state.score}
+              onChange={this.handleChange} />
+          </div>
+        </div>
+        <div className="field">
+          <label
+            className="label"
+            htmlFor="notes">Hole Notes:
+          </label>
+          <div className="control">
+            <textarea
+              className="textarea"
+              name="notes"
+              placeholder="enter notes"
+              value={this.state.notes}
+              onChange={this.handleChange} />
+          </div>
+        </div>
+        <div className="field is-grouped">
+          <div className="control">
+            <button
+              className="button is-primary"
+              type="submit">Save Hole
+            </button>
+          </div>
+          <div className="control">
+            <button className="button is-success">Finish Round</button>
+          </div>
+        </div>
       </form>
     )
   }
