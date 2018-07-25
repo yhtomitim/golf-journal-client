49369492057a4ece34ec8592a545ee37f16883fe
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-24 22:18:08 -0600

    refactor with bulma styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,13 @@ import heroImage from '../header.jpg';
 
 const Header = (props) => {
   return (
-    <header className="App-header">
+    <header className="hero">
+      <div className="hero-body">
+        <div className="container">
       <img src={heroImage} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to Golf Journal</h1>
+      <h1 className="title">Welcome to Golf Journal</h1>
+        </div>
+        </div>
     </header>
   );
 };
