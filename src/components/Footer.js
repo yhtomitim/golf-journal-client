b46f4015876069914f50da9394366c00de588067
yhtomitim <33264110+yhtomitim@users.noreply.github.com>
2018-07-26 11:40:14 -0600

    add norel attribute

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="footer">
       <p className="content has-text-centered">
-        <strong>Golf Journal</strong> by <a href="http://timhurley.me" target="_blank" >Tim Hurley</a>. The source code is licensed <a href="http://opensource.org/licenses/mit-license.php" target="_blank" >MIT</a>. Client-side source code <a href="https://github.com/yhtomitim/golf-journal-client" target="_blank">here</a>. Server-side source code <a href="https://github.com/yhtomitim/golf-journal-api" target="_blank">here</a>.
+        <strong>Golf Journal</strong> by <a href="http://timhurley.me" target="_blank" rel="noopener noreferrer" >Tim Hurley</a>. The source code is licensed <a href="http://opensource.org/licenses/mit-license.php" target="_blank" rel="noopener noreferrer" >MIT</a>. Client-side source code <a href="https://github.com/yhtomitim/golf-journal-client" target="_blank" rel="noopener noreferrer">here</a>. Server-side source code <a href="https://github.com/yhtomitim/golf-journal-api" target="_blank" rel="noopener noreferrer">here</a>.
       </p>
     </div>
   );
